47f9c9827b8566484b3f02ae5c3832e93a7b3acd
Joe Conway <joeconway3@gmail.com>
2018-05-25 09:20:28 -0400

    ready for pub

--- lib/src/coding.dart
+++ lib/src/coding.dart
@@ -2,6 +2,11 @@ import 'package:codable/src/keyed_archive.dart';
 import 'package:meta/meta.dart';
 import 'package:codable/cast.dart' as cast;
 
+/// A base class for encodable and decodable objects.
+///
+/// Types that can read or write their values to a document should extend this abstract class.
+/// By overriding [decode] and [encode], an instance of this type will read or write its values
+/// into a data container that can be transferred into formats like JSON or YAML.
 abstract class Coding {
   Uri referenceURI;
   Map<String, cast.Cast<dynamic>> get castMap => null;
