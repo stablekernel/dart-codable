db9bc8171220274df95ea9cf68a64c5a6ea5187d
Joe Conway <joeconway3@gmail.com>
2018-05-29 15:46:48 -0400

    Adds API docs, adds optional reference following

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 A library for encoding and decoding dynamic data into Dart objects.
 
+## Basic Usage 
+
 Data objects extend `Coding`:
 
 ```dart
@@ -30,10 +32,18 @@ An object that extends `Coding` can be read from JSON:
 ```dart
 final json = json.decode(...);
 final archive = KeyedArchive.unarchive(json);
-final person = new Person()..decode(archive);
+final person = Person()..decode(archive);
 ```
 
-`Coding` objects can encode or decode other `Coding` objects.
+Objects that extend `Coding` may also be written to JSON:
+
+```dart
+final person = Person()..name = "Bob";
+final archive = KeyedArchive.archive(person);
+final json = json.encode(archive);
+```
+
+`Coding` objects can encode or decode other `Coding` objects, including lists of `Coding` objects and maps where `Coding` objects are values. You must provide a closure that instantiates the `Coding` object being decoded.
 
 ```dart
 class Team extends Coding {
@@ -43,19 +53,94 @@ class Team extends Coding {
 
   @override
   void decode(KeyedArchive object) {
-    // must call super
-    super.decode(object);
+    super.decode(object); // must call super
 
-    members = object.decodeObjectList("members");
-    manager = object.decodeObject("manager");
+    members = object.decodeObjects("members", () => Person());
+    manager = object.decodeObject("manager", () => Person());
   }
 
   @override
   void encode(KeyedArchive object) {
     object.encodeObject("manager", manager);
-    object.encodeObjectList("members", members);
+    object.encodeObjects("members", members);
   }
 }
 ```
 
-`Coding` objects may be referred to multiple times in a document without duplicating their structure. See the specification for [JSON Schema](http://json-schema.org) and the `$ref` keyword for more details.
+## Dynamic Type Casting 
+
+Types with primitive type arguments (e.g., `List<String>` or `Map<String, int>`) are a particular pain point when decoding. Override `castMap` in `Coding` to perform type coercion.
+You must import `package:codable/cast.dart as cast` and prefix type names with `cast`.
+
+```dart
+import 'package:codable/cast.dart' as cast;
+class Container extends Coding {  
+  List<String> things;
+
+  @override
+  Map<String, cast.Cast<dynamic>> get castMap => {
+    "things": cast.List(cast.String)
+  };
+  
+
+  @override
+  void decode(KeyedArchive object) {
+    super.decode(object);
+
+    things = object.decode("things");
+  }
+
+  @override
+  void encode(KeyedArchive object) {
+    object.encode("things", things);
+  }
+}
+
+``` 
+
+
+## Document References
+
+`Coding` objects may be referred to multiple times in a document without duplicating their structure. An object is referenced with the `$key` key. 
+For example, consider the following JSON: 
+
+```json
+{
+  "components": {
+    "thing": {
+      "name": "The Thing"
+    }    
+  },
+  "data": {
+    "$ref": "#/components/thing"
+  }
+}
+```
+
+In the above, the decoded value of `data` inherits all properties from `/components/thing`:
+
+```json
+{
+  "$ref": "#/components/thing",
+  "name": "The Thing"
+}
+```
+
+You may create references in your in-memory data structures through the `Coding.referenceURI`. 
+
+```dart
+final person = Person()..referenceURI = Uri(path: "/teams/engineering/manager");
+```
+
+The above person is encoded as:
+
+```json
+{
+  "$ref": "#/teams/engineering/manager"
+}
+```
+
+You may have cyclical references.
+
+See the specification for [JSON Schema](http://json-schema.org) and the `$ref` keyword for more details.
+
--- lib/src/keyed_archive.dart
+++ lib/src/keyed_archive.dart
@@ -5,32 +5,110 @@ import 'package:codable/cast.dart' as cast;
 import 'package:codable/src/list.dart';
 import 'package:codable/src/resolver.dart';
 
+/// A container for a dynamic data object that can be decoded into [Coding] objects.
+///
+/// A [KeyedArchive] is a [Map], but it provides additional behavior for decoding [Coding] objects
+/// and managing JSON Schema references ($ref) through methods like [decode], [decodeObject], etc.
+///
+/// You create a [KeyedArchive] by invoking [KeyedArchive.unarchive] and passing data decoded from a
+/// serialization format like JSON and YAML. A [KeyedArchive] is then provided as an argument to
+/// a [Coding] subclass' [Coding.decode] method.
+///
+///         final json = json.decode(...);
+///         final archive = KeyedArchive.unarchive(json);
+///         final person = Person()..decode(archive);
+///
+/// You may also create [KeyedArchive]s from [Coding] objects so that they can be serialized.
+///
+///         final person = Person()..name = "Bob";
+///         final archive = KeyedArchive.archive(person);
+///         final json = json.encode(archive);
+///
 class KeyedArchive extends Object with MapMixin<String, dynamic> implements Referencable {
-  static KeyedArchive unarchive(Map<String, dynamic> data) {
+  /// Unarchives [data] into a [KeyedArchive] that can be used by [Coding.decode] to deserialize objects.
+  ///
+  /// Each [Map] in [data] (including [data] itself) is converted to a [KeyedArchive].
+  /// Each [List] in [data] is converted to a [ListArchive]. These conversions occur for deeply nested maps
+  /// and lists.
+  ///
+  /// If [allowReferences] is true, JSON Schema references will be traversed and decoded objects
+  /// will contain values from the referenced object. This flag defaults to false.
+  static KeyedArchive unarchive(Map<String, dynamic> data, {bool allowReferences: false}) {
     final archive = new KeyedArchive(data);
-    archive.resolveOrThrow(new ReferenceResolver(archive));
+    if (allowReferences) {
+      archive.resolveOrThrow(new ReferenceResolver(archive));
+    }
     return archive;
   }
 
-  static Map<String, dynamic> archive(Coding root) {
+  /// Archives a [Coding] object into a [Map] that can be serialized into format like JSON or YAML.
+  ///
+  /// Note that the return value of this method, as well as all other [Map] and [List] objects
+  /// embedded in the return value, are instances of [KeyedArchive] and [ListArchive]. These types
+  /// implement [Map] and [List], respectively.
+  ///
+  /// If [allowReferences] is true, JSON Schema references in the emitted document will be validated.
+  /// Defaults to false.
+  static Map<String, dynamic> archive(Coding root, {bool allowReferences: false}) {
     final archive = new KeyedArchive({});
     root.encode(archive);
-    archive.resolveOrThrow(new ReferenceResolver(archive));
+    if (allowReferences) {
+      archive.resolveOrThrow(new ReferenceResolver(archive));
+    }
     return archive;
   }
 
   KeyedArchive._empty();
 
+  /// Use [unarchive] instead.
   KeyedArchive(this._map) {
     _recode();
   }
 
+  /// A reference to another object in the same document.
+  ///
+  /// This value is a path-only [Uri]. Each path segment is a key, starting
+  /// at the document root this object belongs to. For example, the path '/components/all'
+  /// would reference the object as returned by `document['components']['all']`.
+  ///
+  /// Assign values to this property using the default [Uri] constructor and its path argument.
+  /// This property is serialized as a [Uri] fragment, e.g. `#/components/all`.
+  ///
+  /// Example:
+  ///
+  ///         final object = new MyObject()
+  ///           ..referenceURI = Uri(path: "/other/object");
+  ///         archive.encodeObject("object", object);
+  ///
   Uri referenceURI;
 
   Map<String, dynamic> _map;
   Coding _inflated;
   KeyedArchive _objectReference;
 
+  /// Typecast the values in this archive.
+  ///
+  /// Prefer to override [Coding.castMap] instead of using this method directly.
+  ///
+  /// This method will recursively type values in this archive to the desired type
+  /// for a given key. Use this method (or [Coding.castMap]) for decoding `List` and `Map`
+  /// types, where the values are not `Coding` objects.
+  ///
+  /// You must `import 'package:codable/cast.dart' as cast;`.
+  ///
+  /// Usage:
+  ///
+  ///         final dynamicObject = {
+  ///           "key": <dynamic>["foo", "bar"]
+  ///         };
+  ///         final archive = KeyedArchive.unarchive(dynamicObject);
+  ///         archive.castValues({
+  ///           "key": cast.List(cast.String)
+  ///         });
+  ///
+  ///         // This now becomes a valid assignment
+  ///         List<String> key = archive.decode("key");
+  ///
   void castValues(Map<String, cast.Cast> schema) {
     if (schema == null) {
       return;
@@ -73,7 +151,7 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
       if (val is Map) {
         _map[key] = new KeyedArchive(caster.cast(val));
       } else if (val is List) {
-        _map[key] = new ListArchive.fromRaw(val);
+        _map[key] = new ListArchive.from(val);
       } else if (key == r"$ref") {
         if (val is Map) {
           _objectReference = val;
@@ -84,6 +162,9 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     });
   }
 
+  /// Validates [referenceURI]s for this object and any objects it contains.
+  ///
+  /// This method is automatically invoked by both [KeyedArchive.unarchive] and [KeyedArchive.archive].
   @override
   void resolveOrThrow(ReferenceResolver coder) {
     if (referenceURI != null) {
@@ -117,6 +198,15 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     return raw._inflated;
   }
 
+  /// Returns the object associated by [key].
+  ///
+  /// If [T] is inferred to be a [Uri] or [DateTime],
+  /// the associated object is assumed to be a [String] and an appropriate value is parsed
+  /// from that string.
+  ///
+  /// If this object is a reference to another object (via [referenceURI]), this object's key-value
+  /// pairs will be searched first. If [key] is not found, the referenced object's key-values pairs are searched.
+  /// If no match is found, null is returned.
   T decode<T>(String key) {
     var v = _getValue(key);
     if (v == null) {
@@ -132,7 +222,11 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     return v;
   }
 
-
+  /// Returns the instance of [T] associated with [key].
+  ///
+  /// [inflate] must create an empty instance of [T]. The value associated with [key]
+  /// must be a [KeyedArchive] (a [Map]). The values of the associated object are read into
+  /// the empty instance of [T].
   T decodeObject<T extends Coding>(String key, T inflate()) {
     final val = _getValue(key);
     if (val == null) {
@@ -147,6 +241,12 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     return _decodedObject(val, inflate);
   }
 
+  /// Returns a list of [T]s associated with [key].
+  ///
+  /// [inflate] must create an empty instance of [T]. The value associated with [key]
+  /// must be a [ListArchive] (a [List] of [Map]). For each element of the archived list,
+  /// [inflate] is invoked and each object in the archived list is decoded into
+  /// the instance of [T].
   List<T> decodeObjects<T extends Coding>(String key, T inflate()) {
     var val = _getValue(key);
     if (val == null) {
@@ -160,6 +260,12 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     return (val as List<dynamic>).map((v) => _decodedObject(v, inflate)).toList().cast<T>();
   }
 
+  /// Returns a map of [T]s associated with [key].
+  ///
+  /// [inflate] must create an empty instance of [T]. The value associated with [key]
+  /// must be a [KeyedArchive] (a [Map]), where each value is a [T].
+  /// For each key-value pair of the archived map, [inflate] is invoked and
+  /// each value is decoded into the instance of [T].
   Map<String, T> decodeObjectMap<T extends Coding>(String key, T inflate()) {
     var v = _getValue(key);
     if (v == null) {
@@ -194,6 +300,13 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     return json;
   }
 
+  /// Encodes [value] into this object for [key].
+  ///
+  /// If [value] is a [DateTime], it is first encoded as an ISO 8601 string.
+  /// If [value] is a [Uri], it is first encoded to a string.
+  ///
+  /// If [value] is null, no value is encoded and the [key] will not be present
+  /// in the resulting archive.
   void encode(String key, dynamic value) {
     if (value == null) {
       return;
@@ -208,6 +321,10 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     }
   }
 
+  /// Encodes a [Coding] object into this object for [key].
+  ///
+  /// This invokes [Coding.encode] on [value] and adds the object
+  /// to this archive for the key [key].
   void encodeObject(String key, Coding value) {
     if (value == null) {
       return;
@@ -216,14 +333,22 @@ class KeyedArchive extends Object with MapMixin<String, dynamic> implements Refe
     _map[key] = _encodedObject(value);
   }
 
+  /// Encodes list of [Coding] objects into this object for [key].
+  ///
+  /// This invokes [Coding.encode] on each object in [value] and adds the list of objects
+  /// to this archive for the key [key].
   void encodeObjects(String key, List<Coding> value) {
     if (value == null) {
       return;
     }
 
-    _map[key] = new ListArchive.fromRaw(value.map((v) => _encodedObject(v)).toList());
+    _map[key] = new ListArchive.from(value.map((v) => _encodedObject(v)).toList());
   }
 
+  /// Encodes map of [Coding] objects into this object for [key].
+  ///
+  /// This invokes [Coding.encode] on each value in [value] and adds the map of objects
+  /// to this archive for the key [key].
   void encodeObjectMap<T extends Coding>(String key, Map<String, T> value) {
     if (value == null) {
       return;
